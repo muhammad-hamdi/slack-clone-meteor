963424ad42ffd67d8a2869e669c7bf67a0d60273
Muhammad Hamdi <prime.energy7@gmail.com>
2017-09-14 13:12:01 +0200

    append messages onkeypress

--- client/main.js
+++ client/main.js
@@ -1,22 +1,27 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
-import './main.html';
+import './room.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+Template.messages.helpers({
+	messages: [
+		{ text: "All these messages" },
+	    { text: "Uses the same template" },
+	    { text: "But have a different data context" },
+	    { text: "It's why these messages are all different!" },
+	    { text: "Hey!" },
+	    { text: "What's up man!" },
+	    { text: "Hello" }
+	]
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+Template.footer.events({
+	'keypress input': function(e){
+		if (e.charCode === 13){
+			e.stopPropagation();
+			$('.message-history').append('<div class="message"><a href="" class="message_profile-pic"></a><a href="" class="message_username">scotch</a><span class="message_timestamp">1:31 AM</span><span class="message_star"></span><span class="message_content">' + $('.input-box_text').val() + '</span></div>');
+			$('.input-box_text').val("");
+			return false;
+		}
+	}
+})
