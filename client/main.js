08571611d742ca84d764be7ece10391c7aedb569
Muhammad Hamdi <prime.energy7@gmail.com>
2017-09-14 14:48:30 +0200

    added messages collection, and worked simple realtime chat

--- client/main.js
+++ client/main.js
@@ -1,27 +1,27 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
+import {Messages} from '../lib/collections/messages';
 
 import './room.html';
 
 Template.messages.helpers({
-	messages: [
-		{ text: "All these messages" },
-	    { text: "Uses the same template" },
-	    { text: "But have a different data context" },
-	    { text: "It's why these messages are all different!" },
-	    { text: "Hey!" },
-	    { text: "What's up man!" },
-	    { text: "Hello" }
-	]
+	messages: Messages.find({}),
 });
 
 Template.footer.events({
 	'keypress input': function(e){
-		if (e.charCode === 13){
-			e.stopPropagation();
-			$('.message-history').append('<div class="message"><a href="" class="message_profile-pic"></a><a href="" class="message_username">scotch</a><span class="message_timestamp">1:31 AM</span><span class="message_star"></span><span class="message_content">' + $('.input-box_text').val() + '</span></div>');
-			$('.input-box_text').val("");
-			return false;
-		}
+		var inputVal = $('.input-box_text').val();
+
+	    if(!!inputVal) {
+	      var charCode = (typeof e.which == "number") ? e.which : e.keyCode;
+
+	      if (charCode == 13) {
+	        e.stopPropagation();
+	        Messages.insert({text: $('.input-box_text').val()});
+	        $('.input-box_text').val("");
+	        return false;
+	      }
+	      
+	    }
 	}
 })
