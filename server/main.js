276fba72b18d5fe51d54e9d26a7e399d0373070d
MuhammadHamdi1 <prime.energy7@gmail.com>
2017-09-18 19:52:53 +0200

    moved channel seeding to the main meteor file and used native bcrypt instead of js version

--- server/main.js
+++ server/main.js
@@ -1,5 +1,12 @@
 import { Meteor } from 'meteor/meteor';
+import {Channels} from '../lib/collections/channels';
 
 Meteor.startup(() => {
-
+	Channels.remove({});
+	Channels.insert({
+	  name: "general"
+	});
+	Channels.insert({
+	  name: "random"
+	});
 });
